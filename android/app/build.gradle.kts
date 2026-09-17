5f17c977db23d08dd2da521c1657d32aac59d50b
Nithin J <sathesh95@gmail.com>
2026-09-17 15:37:58 +0530

    Add missing launcher icons and set compileSdk to 34

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.clipboardsync.android"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.clipboardsync.android"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0.0"
 
